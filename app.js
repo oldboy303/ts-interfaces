50bb8a423cca2bfd60b6e9718bc39680c534e7e3
San Guyer <sanguyer@gmail.com>
2018-03-01 17:08:56 -0500

    Done

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Circle = require("./math/circle");
-var rectangle_1 = require("./math/rectangle");
-console.log(Circle.PI);
-console.log(Circle.calculateCirumcumference(10));
-console.log(rectangle_1.default(20, 50));
+function greet(person) {
+    console.log("Hello " + person.firstName);
+}
+function changeName(person) {
+    person.firstName = 'Bob';
+}
+var person = {
+    firstName: 'San',
+    age: 42,
+    greet: function (lastName) {
+        console.log("Hello " + this.firstName + " " + lastName);
+    }
+};
+// greet({ firstName: 'San', age: 42 });
+changeName(person);
+greet(person);
+person.greet('Will');
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hello " + this.firstName + " " + lastName);
+    };
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = 'Sani';
+greet(myPerson);
+myPerson.greet('Boy');
+var myMultiFunc;
+myMultiFunc = function (val1, val2) { return val1 * val2; };
+console.log(myMultiFunc(10, 2));
+var oldPerson = {
+    age: 101,
+    firstName: 'Cengiz',
+    greet: function (lastName) {
+        console.log("Hello " + this.firstName + " " + lastName);
+    }
+};
+oldPerson.greet('Guyer');
--- app.ts
+++ app.ts
@@ -1,6 +1,63 @@
-import * as Circle from "./math/circle";
-import calc from "./math/rectangle";
+interface NamedPerson {
+    firstName: string;
+    age?: number;
+    [propName: string]: any;
+    greet(lastName: string): void;
+}
 
-console.log(Circle.PI);
-console.log(Circle.calculateCirumcumference(10));
-console.log(calc(20, 50));
+function greet(person: NamedPerson): void {
+    console.log(`Hello ${ person.firstName}`);
+}
+
+function changeName(person: NamedPerson): void {
+    person.firstName = 'Bob';
+}
+
+let person = {
+    firstName: 'San',
+    age: 42,
+    greet(lastName: string): void {
+        console.log(`Hello ${ this.firstName} ${ lastName }`);
+    }
+};
+
+// greet({ firstName: 'San', age: 42 });
+changeName(person);
+greet(person);
+person.greet('Will');
+
+class Person implements NamedPerson {
+    firstName: string;
+    greet(lastName: string) {
+        console.log(`Hello ${ this.firstName} ${ lastName }`);
+    }
+}
+
+let myPerson = new Person();
+myPerson.firstName = 'Sani';
+greet(myPerson);
+myPerson.greet('Boy');
+
+// Function Types
+interface MultiplyFunc {
+    (num1: number, num2: number): number;
+}
+
+let myMultiFunc: MultiplyFunc;
+myMultiFunc = (val1: number, val2: number) => val1 * val2;
+
+console.log(myMultiFunc(10, 2));
+
+// Interface Inheritance
+interface AgedPerson extends NamedPerson {
+    age: number;
+}
+
+let oldPerson: AgedPerson = {
+    age: 101,
+    firstName: 'Cengiz',
+    greet(lastName: string): void {
+        console.log(`Hello ${ this.firstName } ${ lastName }`);
+    }
+};
+oldPerson.greet('Guyer');
